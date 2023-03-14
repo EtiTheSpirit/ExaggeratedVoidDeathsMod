f9a825243dd3884af3f696e65418ea1e225b1928
EtiTheSpirit <subzeroxan@gmail.com>
2023-03-14 08:23:30 -0500

    Add project files.

---
+++ ExaggeratedVoidDeathsMod/ExaggeratedVoidDeathsPlugin.cs
@@ -0,0 +1,66 @@
+using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using Xan.ROR2VoidPlayerCharacterCommon;
+using Xan.ROR2VoidPlayerCharacterCommon.DamageBehavior;
+
+namespace ExaggeratedVoidDeathsMod {
+	[BepInDependency(R2API.R2API.PluginGUID)]
+	[BepInDependency("Xan.VoidPlayerCharacterCommon")]
+	[R2APISubmoduleDependency(nameof(DamageAPI))]
+	[BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+	[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.EveryoneNeedSameModVersion)]
+	public class ExaggeratedVoidDeathsPlugin : BaseUnityPlugin {
+		public const string PLUGIN_GUID = PLUGIN_AUTHOR + "." + PLUGIN_NAME;
+		public const string PLUGIN_AUTHOR = "Xan";
+		public const string PLUGIN_NAME = "ExaggeratedVoidDeaths";
+		public const string PLUGIN_VERSION = "1.0.0";
+		
+		public void Awake() {
+			Log.Init(Logger);
+			IL.RoR2.HealthComponent.TakeDamage += SetUsesVoidDeath;
+			On.RoR2.HealthComponent.TakeDamage += AfterTakingDamage;
+		}
+
+		private static bool _lastDamageWasVoidDeathFromLenses = false;
+
+		private static void SetUsesVoidDeath(ILContext il) {
+			ILCursor cursor = new ILCursor(il);
+			cursor.GotoNext(
+				instruction => instruction.MatchLdarg(1),
+				instruction => instruction.MatchDup(),
+				instruction => instruction.MatchLdfld("RoR2.DamageInfo", "damageType"),
+				instruction => instruction.MatchLdcI4(0x10000),
+				instruction => instruction.MatchOr(),
+				instruction => instruction.MatchStfld("RoR2.DamageInfo", "damageType")
+			);
+			cursor.EmitDelegate(SetLastDamageWasVoidDeath);
+		}
+
+		private static void SetLastDamageWasVoidDeath() {
+			_lastDamageWasVoidDeathFromLenses = true;
+		}
+
+		private void AfterTakingDamage(On.RoR2.HealthComponent.orig_TakeDamage originalMethod, HealthComponent @this, DamageInfo damageInfo) {
+			bool wasAliveBefore = @this.alive;
+			originalMethod(@this, damageInfo);
+			if (!_lastDamageWasVoidDeathFromLenses && !@this.alive && wasAliveBefore) {
+				if (XanVoidAPI.ShouldShowVoidDeath(damageInfo)) {
+					EffectManager.SpawnEffect(
+						VoidEffects.SilentVoidCritDeathEffect,
+						new EffectData {
+							origin = @this.body.corePosition,
+							scale = @this.body ? @this.body.radius : 1f
+						},
+						true
+					);
+				}
+			}
+		}
+	}
+}
---
+++ ModReleasePackage/README.md
@@ -0,0 +1,10 @@
+# Exaggerated Void Deaths
+
+You know what looks cool? That gorgeous, sharp implosion effect caused by the Lost Seer's Lenses. I am so fond of the VFX that I even added it to my Void player character mods when the player's black hole kills stuff.
+
+People really liked that, thought it looked kickass to see that slew of damage numbers violently shoot out from the site of the implosion.
+
+So here's a mod that does that. Fear not, your ears will not implode with the 50 lemurians you just deleted. This variation of the VFX is silent, allowing the natural implosion sound of Void enemies to do the work of providing auditory feedback to you.
+
+# Interested in my mods?
+I have a Discord server dedicated to my mods for all the games that I make mods for! You can join it [here](https://discord.gg/YGJ7a44UEE).
